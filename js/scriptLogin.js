697b515098e4bc7a52af652d9c85096050ec38e2
João Celso <joaocelso0205@gmail.com>
2023-05-23 09:44:23 -0300

    Página Principal 2

---
+++ js/scriptLogin.js
@@ -0,0 +1,18 @@
+var btnEntrar = document.querySelector(".entrar-btn")
+var senhaErro = document.getElementById("senhaErrada")
+var msgErro = document.getElementById("msgErro")
+btnEntrar.addEventListener("click", btnEntrarFunction)
+function btnEntrarFunction(){
+    var emailValue = document.getElementById("email").value;
+    var senhaValue = document.getElementById("senha").value;
+    if (emailValue == 'email@gmail.com' && senhaValue == 'senha123'){
+        window.location.href= "mainLogado.html"
+        console.log("Teste")
+    }
+    if (senhaValue != 'senha123' && emailValue == 'email@gmail.com'){
+        senhaErro.classList.add("active")
+    }
+    else{
+        msgErro.classList.add("active")
+    }
+}
