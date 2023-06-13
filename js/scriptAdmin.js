d3c8f8d25d36253298e62e27acc338ecf7286d87
João Celso <joaocelso0205@gmail.com>
2023-06-13 08:37:49 -0300

    AreaRestrita

--- js/scriptAdmin.js
+++ js/scriptAdmin.js
@@ -13,16 +13,69 @@ function menuHambur(){
         itensM.classList.remove("active")
     }
 }
-var lucros = document.querySelector("#lucros")
-var resultados = document.querySelector("#resultados")
-var acessos = document.querySelector("#cessos")
-lucros.addEventListener('click', lucrosF)
-function lucrosF(){
-    if (resultados.classList.contains('active') && acessos.classList.contains("active")){
-        resultados.classList.remove("active")
-        acessos.classList.remove('active')
+var lucro = document.getElementById('lucros')
+var myTable = document.querySelector(".myTables")
+lucro.addEventListener('click', () => {
+    myTable.classList.toggle('active')
+    console.log('teste')
+})
+    
+
+let data = [
+    {"dtRecord" : "2023-05-01 10:10:35",
+    "nome": "Ageu Duarte Junior",
+    "eMail": "ageu@ageu.com.br",
+    "assunto": "Dúvidas",
+    "mensagem": "Mensagem 1 | SENAI - SESI - Alunos do curso desenvolvimento de sistemas"
+    },
+
+    {"dtRecord" : "2023-05-01 12:15:35",
+    "nome": "Ageu Duarte Junior",
+    "eMail": "ageu@ageu.com.br",
+    "assunto": "Opções de Entrega",
+    "mensagem": "Mensagem 2 | SENAI - SESI - Alunos do curso desenvolvimento de sistemas"
+    },
+
+    {"dtRecord" : "2023-05-01 13:15:35",
+    "nome": "Ageu Duarte Junior",
+    "eMail": "ageu@ageu.com.br",
+    "assunto": "Telefone de Contato",
+    "mensagem": "Mensagem 3 | SENAI - SESI - Alunos do curso desenvolvimento de sistemas"
+    },
+    
+    {"dtRecord" : "2023-05-01 14:15:35",
+    "nome": "Ageu Duarte Junior",
+    "eMail": "ageu@ageu.com.br",
+    "assunto": "Endereço para Retirada",
+    "mensagem": "Mensagem 4 | SENAI - SESI - Alunos do curso desenvolvimento de sistemas"
+    },
+
+    {"dtRecord" : "2023-05-01 15:15:35",
+    "nome": "Ageu Duarte Junior",
+    "eMail": "ageu@ageu.com.br",
+    "assunto": "Oportunidades de trabalho",
+    "mensagem": "Mensagem 5 | SENAI - SESI - Alunos do curso desenvolvimento de sistemas"
     }
-    else{
-        lucros.classList.add('active')
-    }
+];
+
+let table = document.getElementById("myTable");
+let rowCount = table.rows.length;
+
+for (let i = 0; i < data.length; i++) {
+    const reg = data[i];
+
+    let row = table.insertRow(rowCount);
+    let cell1 = row.insertCell(0);
+    let cell2 = row.insertCell(1);
+    let cell3 = row.insertCell(2);
+    let cell4 = row.insertCell(3);
+    let cell5 = row.insertCell(4);
+        
+    cell1.innerHTML = data[i].dtRecord;
+    cell2.innerHTML = data[i].nome;
+    cell3.innerHTML = data[i].eMail;
+    cell4.innerHTML = data[i].assunto;
+    cell5.innerHTML = data[i].mensagem;
+
+    rowCount++;    
 }
